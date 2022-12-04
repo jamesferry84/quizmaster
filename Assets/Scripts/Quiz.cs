83edb2e25c7fc62e47630eccf88d347c84d38f4e
jamesferry84 <jamesferry84@hotmail.com>
2022-12-04 15:01:07 +0000

    macbook updates

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+    TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    void Start()
+    {
+        
+    }
+}
